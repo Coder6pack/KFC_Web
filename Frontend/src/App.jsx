2def69009cddd3ee7223bcc042d14d3e7daf5abb
ThieenNguyen <0306211519@caothang.edu.vn>
2023-12-14 12:54:07 +0700

    add dev2

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import Slideshow from './components/slideshow/slideshow'
 import Partyform from './components/Party/Partyform'
-;<script src='https://kit.fontawesome.com/yourcode.js' crossorigin='anonymous'></script>
 const slides = [
   'https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/GiangSinh.webp?v=gXDOPg',
   'https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/BTLD.webp?v=gXDOPg',
