26855e1d1a24150ee4dc0abe6b6455fc8f222849
ThieenNguyen <0306211519@caothang.edu.vn>
2023-12-14 12:38:17 +0700

    Slideshow_Party

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import Card from './components/Card'
-
+import Slideshow from './components/slideshow/slideshow'
+import Partyform from './components/Party/Partyform'
+;<script src='https://kit.fontawesome.com/yourcode.js' crossorigin='anonymous'></script>
+const slides = [
+  'https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/GiangSinh.webp?v=gXDOPg',
+  'https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/BTLD.webp?v=gXDOPg',
+  'https://static.kfcvietnam.com.vn/images/content/home/carousel/lg/GaQueKem.webp?v=gXDOPg'
+]
 function App() {
   return (
     <div>
-      <Card />
+      <Partyform />
+      <br />
+      <Slideshow autoslide={true}>
+        {slides.map((s) => (
+          <img src={s} />
+        ))}
+      </Slideshow>
     </div>
   )
 }
