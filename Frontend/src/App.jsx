73d1911bd73db2e8327baa19f07b12660c2d67ea
ThieenNguyen <0306211519@caothang.edu.vn>
2024-01-25 22:38:50 +0700

    commit lancuoicung

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import AdminPage from "./components/AdminPage";
-
+import Header from "./components/Header"
 export default function App() {
   return (
     <div>
+      <Header/>
       <AdminPage/>
     </div>
   )
