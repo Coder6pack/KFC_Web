f6adbd9540ebfde3e88ef07a8ca60786bfb91d2a
Coder6pack <105229432+Coder6pack@users.noreply.github.com>
2024-01-26 20:35:12 +0700

    dev1 fix v1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,3 @@
-import AdminPage from "./components/AdminPage";
-import Header from "./components/Header"
 export default function App() {
-  return (
-    <div>
-      <Header/>
-      <AdminPage/>
-    </div>
-  )
+  return <div></div>
 }
