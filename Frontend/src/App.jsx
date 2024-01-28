50d5c5024d8e087ddf6d44b7166f39c6abe056d8
ThieenNguyen <0306211519@caothang.edu.vn>
2024-01-29 01:05:04 +0700

    committtttttt

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,12 @@
-import { Route, Routes } from 'react-router-dom'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Home from './pages/Home'
-import MenuPage from './pages/MenuPage'
-import DetailPage from './pages/DetailPage'
-import ShoppingCartPage from './pages/ShoppingCartPage'
-import PaymentPage from './pages/PaymentPage'
-import NotFoundPage from './pages/NotFoundPage'
+import Login from './components/Login'
+import Payment from './components/Payment'
+import AdminPage from './components/AdminPage'
 export default function App() {
   return (
     <div>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/menu' element={<MenuPage />} />
-        <Route path='/menu/:id/detail' element={<DetailPage />} />
-        <Route path='/shopping-cart' element={<ShoppingCartPage />} />
-        <Route path='/payment' element={<PaymentPage />} />
-        <Route path='/*' element={<NotFoundPage />} />
-      </Routes>
-      <Footer />
+      <Login/>
+      <Payment/>
+      <AdminPage/>
     </div>
   )
 }
