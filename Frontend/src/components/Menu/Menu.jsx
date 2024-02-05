98ce082b9f2649506f0e6b3ed5250a8ef8308258
Coder6pack <105229432+Coder6pack@users.noreply.github.com>
2024-02-05 23:47:54 +0700

    fix bug 5/2

--- Frontend/src/components/Menu/Menu.jsx
+++ Frontend/src/components/Menu/Menu.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom'
 import Foods from '../Foods'
-
 export default function Menu() {
   return (
     <div className='flex flex-wrap justify-center gap-3 mb-100'>
